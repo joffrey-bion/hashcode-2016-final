be92f87bdebf2cf053a235af6f320c5a515049b5
jbion <joffrey.bion@amadeus.com>
2016-03-19 16:25:08 +0100

    add test for Coord.move()

--- src/test/java/org/ohutouch/hashcode/CoordsTest.java
+++ src/test/java/org/ohutouch/hashcode/CoordsTest.java
@@ -8,6 +8,55 @@ import static org.ohutouch.hashcode.Simulation.*;
 public class CoordsTest {
 
     @Test
+    public void testMove() {
+        int[] pos = new int[2];
+        boolean invertedVelocity;
+
+        pos[LATITUDE] = 1000;
+        pos[LONGITUDE] = 2000;
+
+        invertedVelocity = Coord.move(pos, 100, 150);
+        Assert.assertEquals(1100, pos[LATITUDE]);
+        Assert.assertEquals(2150, pos[LONGITUDE]);
+        Assert.assertFalse(invertedVelocity);
+
+        pos[LATITUDE] = 1000;
+        pos[LONGITUDE] = 2000;
+
+        invertedVelocity = Coord.move(pos, -100, -150);
+        Assert.assertEquals(900, pos[LATITUDE]);
+        Assert.assertEquals(1850, pos[LONGITUDE]);
+        Assert.assertFalse(invertedVelocity);
+
+        pos[LATITUDE] = 1000;
+        pos[LONGITUDE] = 179 * 3600;
+
+        invertedVelocity = Coord.move(pos, 0, 3600);
+        Assert.assertEquals(1000, pos[LATITUDE]);
+        Assert.assertEquals(-180 * 3600, pos[LONGITUDE]);
+        Assert.assertFalse(invertedVelocity);
+
+        invertedVelocity = Coord.move(pos, 0, 3600);
+        Assert.assertEquals(1000, pos[LATITUDE]);
+        Assert.assertEquals(-179 * 3600, pos[LONGITUDE]);
+        Assert.assertFalse(invertedVelocity);
+    }
+
+    @Test
+    public void testMoveAtPole() {
+        int[] pos = new int[2];
+        boolean invertedVelocity;
+
+        pos[LATITUDE] = 89 * 3600;
+        pos[LONGITUDE] = 10 * 3600;
+
+        invertedVelocity = Coord.move(pos, 2 * 3600, 0);
+        Assert.assertEquals(89 * 3600, pos[LATITUDE]);
+        Assert.assertEquals(-170 * 3600, pos[LONGITUDE]);
+        Assert.assertTrue(invertedVelocity);
+    }
+
+    @Test
     public void testDifference() {
         int[] pos1 = new int[2];
         int[] pos2 = new int[2];
